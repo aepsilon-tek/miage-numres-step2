c14b2f080d18c653a333f3d2a05b2b2fd22ffdef
boisseau <sboisseau@aepsilon.com>
2024-11-28 18:46:38 +0100

    Implementation v2

---
+++ api-quarkus-v2/src/main/resources/InitH2.sql
@@ -0,0 +1,8 @@
+Insert into Question (id,label) values (1,'Test de question 1 ?');
+Insert into Question (id,label) values (2,'Test de question 2 ?');
+
+
+Insert into Proposal(id,label,correct,QUESTION_ID) values (1,'rep 1',1,1);
+Insert into Proposal(id,label,correct,QUESTION_ID) values (2,'rep 2',0,1);
+Insert into Proposal(id,label,correct,QUESTION_ID) values (3,'rep 3',1,2);
+Insert into Proposal(id,label,correct,QUESTION_ID) values (4,'rep 4',0,2);
